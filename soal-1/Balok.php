5adbffa987383f8c4c6a791a6897d5991160e19d
hanif.khalilurrahim3@gmail.com <hanif.khalilurrahim3@gmail.com>
2024-10-03 12:13:16 +0800

    phase 1

--- soal-1/Balok.php
+++ soal-1/Balok.php
@@ -2,23 +2,67 @@
 class Balok
 {
     // deklarasikan atribut-atribut kelas Balok
-
+    private $panjang ;
+    private $lebar ;
+    private $tinggi ;
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($panjang, $lebar, $tinggi)
+    {
+        $this -> panjang = $panjang;
+        $this -> lebar = $lebar;
+        $this -> tinggi = $tinggi;
+    }
 
+    public function setpanjang($panjang) {
+        $this->panjang = $panjang;
+    }
+
+    public function setlebar($lebar) {
+        $this->lebar = $lebar;
+    }
+
+    public function settinggi($tinggi) {
+        $this->tinggi = $tinggi;
+    }
+
+    public function getpanjang() {
+        return $this->panjang;
+    }
+
+    public function getlebar() {
+        return $this->lebar;
+    }
+
+    public function gettinggi() {
+        return $this->tinggi;
+    }
 
     // definisikan metode menghitung volume, luas permukaan, 
+    public function hitungVolume() {
+        return $this->panjang * $this->lebar * $this->tinggi;
+    }
+
+    public function hitungLuasPermukaan() {
+        return 2 * (($this->panjang * $this->lebar) + ($this->panjang * $this->tinggi) + ($this->lebar * $this->tinggi));
+    }
 }
 
 $balok_1 = new Balok(4, 8, 5);
 $balok_2 = new Balok(3, 10, 12);
-
+echo "block 1 .<br>";
+echo "volume : " . $balok_1 -> hitungVolume() . "<br>";
+echo "luaspermuakaan : " . $balok_1 -> hitungLuasPermukaan() . "<br>";
+echo "block 2 .<br>";
+echo "volume : " . $balok_2 -> hitungVolume() . "<br>";
+echo "luaspermuakaan : " . $balok_2 -> hitungLuasPermukaan() . "<br>";
 // contoh output: Balok
 
 // Balok 1
 // Volume: 160
-// Luas Permukaan: 184
+// Luas Permukaan: 184 
 
 // Balok 2
 // Volume: 360
 // Luas Permukaan: 372
+?>
--- soal-2/Siswa.php
+++ soal-2/Siswa.php
@@ -3,15 +3,59 @@
 class Siswa
 {
     // deklarasikan atribut-atribut kelas Siswa
-
+    private $nama;
+    private $nisn;
+    private $alamat;
+    private $jurusan;
+    private $tanggal_lahir;
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($nama, $nisn, $alamat, $jurusan, $tanggal_lahir)
+    {
+        $this -> nama = $nama;
+        $this -> nisn = $nisn;
+        $this -> alamat = $alamat;
+        $this -> jurusan = $jurusan;
+        $this -> tanggal_lahir = $tanggal_lahir;
+    }
 
+    public function setnama($nama) {
+        $this->nama = $nama;
+    }
+
+    public function setnisn($nisn) {
+        $this->nisn = $nisn;
+    }
+
+    public function setalamat($alamat) {
+        $this->alamat = $alamat;
+    }
+
+    public function setjurusan($jurusan) {
+        $this->jurusan = $jurusan;
+    }
+
+    public function getnama() {
+        return $this->nama;
+    }
+
+    public function getnisn() {
+        return $this->nisn;
+    }
+
+    public function getalamat() {
+        return $this->alamat;
+    }
+
+    public function getjurusan() {
+        return $this->jurusan;
+    }
 
     // buat metode untuk menghitung umur siswa
     public function hitungUmur() {
-
-
+        $tanggal_lahir = new datetime($this->tanggal_lahir);
+        $tanggal_sekarang = new datetime();
+        $umur = $tanggal_lahir -> diff($tanggal_sekarang);
         return $umur;
     }
 }
